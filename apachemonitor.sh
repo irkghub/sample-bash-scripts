e4afe122873ca147caa63b916dc1b7e342987167
irkghub <bkbhandari99@gmail.com>
2023-12-25 09:47:17 +0545

    Apache Status Check Added

--- apachemonitor.sh
+++ apachemonitor.sh
@@ -11,10 +11,11 @@ else
     echo "Apache HTTP Server is not running"
     echo 
     echo "Starting the Server"
-    sudo systemctl start httpd
-    
-    echo "Displaying server status: active"
-
+    sudo systemctl start httpd &> /dev/null
+    if [ $? -eq 0 ]
+        echo "Successfully started the Apache HTTP Server!"
+    else
+        echo "Failed to start Apache HTTP Server!"
 
 fi
 
