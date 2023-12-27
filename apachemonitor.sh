285c31451bf028b27bbd9714f2cd59dc702f0f20
irkghub <bkbhandari99@gmail.com>
2023-12-27 18:46:27 +0545

    issues fixed with conditional error

--- apachemonitor.sh
+++ apachemonitor.sh
@@ -2,20 +2,17 @@
 
 ls /var/run/httpd/httpd.pid &> /dev/null
 
-if [ $? -eq 0 ]
-
-then
+if [ $? -eq 0 ]; then
     echo "Apache HTTP Server is running"
-
 else
     echo "Apache HTTP Server is not running"
-    echo 
+    echo
     echo "Starting the Server"
     sudo systemctl start httpd &> /dev/null
-    if [ $? -eq 0 ]
+    if [ $? -eq 0 ]; then
         echo "Successfully started the Apache HTTP Server!"
     else
         echo "Failed to start Apache HTTP Server!"
-
+    fi
 fi
 
